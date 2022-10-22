b74c52424c1433f3c2efe3f6965f627b77620a1d
Alexander Klindyuk <info@binom-r.ru>
2022-10-22 15:56:24 +0300

    Занятие 2. Дракон

---
+++ dragon/examples.py
@@ -0,0 +1,15 @@
+# a = 5
+# b = 7
+# print(a + b)
+# a = a + 19
+# print(a)
+
+# a = input()
+# b = input()
+# # a и b -- строки!
+# print(a + b)
+# print(a * 5)
+
+a = int(input())
+b = int(input())
+print(a + b)
