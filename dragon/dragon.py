4ed0ea0a4df587c9a4b565c91c81cff0f1f4b43e
Alexander Klindyuk <info@binom-r.ru>
2022-10-29 15:56:23 +0300

    Занятие 3

--- dragon/dragon.py
+++ dragon/dragon.py
@@ -1,18 +1,28 @@
 import random, time
 
-secret = random.randint(1, 2)
-print('Перед вами две пещеры. В одной из них спрятано сокровище, в другой сидит голодный дракон. Какую пещеру вы выберете (1 или 2)?')
-userChoice = int(input())
+gameOn = True
+while gameOn:
+    secret = random.randint(1, 2)
+    print('Перед вами две пещеры. В одной из них спрятано сокровище, в другой сидит голодный дракон. Какую пещеру вы выберете (1 или 2)?')
+    userChoice = input()
 
-time.sleep(1)
-print('Вы заходите в пещеру...')
-time.sleep(2)
-print('В пещере темно и сыро...')
-time.sleep(2)
-print('Вы включаете фонарь и видите...')
-time.sleep(2)
+    while userChoice != '1' and userChoice != '2':
+        print('Недопустимый ввод. Выберите 1 или 2.')
+        userChoice = input()
 
-if secret == userChoice:
-    print('Сундук с сокровищами. Теперь вы богаты!')
-else:
-    print('Глаза огромного дракона! К сожалению, вы проиграли...')
+    time.sleep(1)
+    print('Вы заходите в пещеру...')
+    time.sleep(2)
+    print('В пещере темно и сыро...')
+    time.sleep(2)
+    print('Вы включаете фонарь и видите...')
+    time.sleep(2)
+
+    userChoice = int(userChoice)
+
+    if secret == userChoice:
+        print('Сундук с сокровищами. Теперь вы богаты!')
+    else:
+        print('Глаза огромного дракона! К сожалению, вы проиграли...')
+    
+    
--- dragon/examples.py
+++ dragon/examples.py
@@ -1,15 +1,8 @@
-# a = 5
-# b = 7
-# print(a + b)
-# a = a + 19
-# print(a)
-
-# a = input()
-# b = input()
-# # a и b -- строки!
-# print(a + b)
-# print(a * 5)
-
 a = int(input())
 b = int(input())
-print(a + b)
+if a > b:
+    print('Первое больше')
+elif a == b:
+    print('Равны')
+else:
+    print('Второе больше')
